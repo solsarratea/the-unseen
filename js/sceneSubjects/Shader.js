6b5997c6d1bfd33b65b198610b9c236551278162
Unknown <ssarratea@manas.tech>
2020-10-30 00:45:42 -0300

    Add textures

--- js/sceneSubjects/Shader.js
+++ js/sceneSubjects/Shader.js
@@ -1,10 +1,13 @@
 function Shader(scene) {
     var geometry = new THREE.PlaneBufferGeometry( 2,2);
+    var texture  = new THREE.TextureLoader().load("textures/q2.jpg");
 
     var material = new THREE.ShaderMaterial( {
         uniforms: {
             resolution : { type : 'v2', value : new THREE.Vector2( window.innerWidth, window.innerHeight) },
             decode : { value: true},
+            tex0: { type: "t", value: texture },
+            time: { type: "f", value: 0},
          },
         fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
         vertexShader: document.getElementById( 'vertexShader' ).textContent
@@ -14,6 +17,7 @@ function Shader(scene) {
     scene.add( shaderObject );
 
     this.update = function(time, sceneControls) {
-       material.uniforms.decode.value = sceneControls.decode;
+        material.uniforms.decode.value = sceneControls.decode;
+        material.uniforms.time.value = time;
     }
 }
